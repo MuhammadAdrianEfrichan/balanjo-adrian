53ffb32baaff7d4e881df53b31ded442f9726803
MuhammadAdrianEfrichan <adrianefrich@gmail.com>
2026-09-13 13:21:35 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import { RouterProvider } from "react-router-dom"
+import { useEffect } from "react"
 import router from "./routes"
 
 function App() {
+  useEffect(() => {
+    const unsubscribe = router.subscribe((state) => {
+      if (state.navigation.state === "idle") {
+        window.scrollTo({ top: 0, left: 0, behavior: "auto" })
+      }
+    })
+
+    return unsubscribe
+  }, [])
+
   return   <RouterProvider router={router} />
 }
 
